58a179306ef18d9b850243c7ba086e587fa3bf22
Shawn Lin <45999699+shawnlin0201@users.noreply.github.com>
2020-03-04 02:30:32 +0800

    Update main.js

--- module/main/main.js
+++ module/main/main.js
@@ -22,9 +22,14 @@ module.exports.loop = function () {
             {align: 'left', opacity: 0.8});
     }
 
-// harvester controller
+// Creeps controller
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
