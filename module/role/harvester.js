fe73e0a5ae8d75482a8ba8b835697c0005598730
Shawn Lin <45999699+shawnlin0201@users.noreply.github.com>
2020-03-04 02:57:46 +0800

    Update harvester.js

--- module/role/harvester.js
+++ module/role/harvester.js
@@ -1,27 +1,34 @@
-var roleHarvester = {
+
+var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0) {
+    // Give Spawn
+        if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0]);
             }
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
             }
         }
-	}
+
+    // Give Room Controller
+        // if(creep.store[RESOURCE_ENERGY] == 0) {
+        //     var sources = creep.room.find(FIND_SOURCES);
+        //     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        //         creep.moveTo(sources[0]);
+        //     }
+        // }
+        // else {
+        //     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        //         creep.moveTo(creep.room.controller);
+        //     }
+        // }
+    }
 };
 
-module.exports = roleHarvester;
+module.exports = roleUpgrader;
